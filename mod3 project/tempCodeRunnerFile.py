1ac8e052abb0aadccc102db9fcefc8e5f3d4c712
Raiyan K <96327738+Amerax@users.noreply.github.com>
2024-08-03 22:39:46 -0400

    Add files via upload

---
+++ mod3 project/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+ # # Read cancer-risk data set, select columns A, B, C, E, and L, then sort by column E in descending order
+    # cancer_risk_table = read_csv_file("cancer_risk05_v4_county.csv")
+    # col_indices = [0, 1, 2, 4, 11]
+    # trimmed_risk_table = select_columns(cancer_risk_table, col_indices)
+    # sort_by_column(trimmed_risk_table, 4)
+    # write_csv_file(trimmed_risk_table, "cancer_risk_trimmed.csv")
+    
+    # # Load our file "cancer_risk_trimmed_solution.csv" and compare with your solution
+    # trimmed_risk_solution = read_csv_file("cancer_risk_trimmed_solution.csv")
+    
